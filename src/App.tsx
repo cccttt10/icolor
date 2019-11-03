cec8eae478c9b58f6c2a2e4c351d47a6d1c58e66
Chuntong Gao <heihegao@gmail.com>
2019-11-02 21:19:23 -0700

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import { generatePalette } from './colorHelpers';
 import Palette from './Palette';
 import { starterPalettes } from './starterPalettes';
@@ -7,9 +7,13 @@ import { starterPalettes } from './starterPalettes';
 class App extends Component {
     render() {
         return (
-            <div>
-                <Palette palette={generatePalette(starterPalettes[4])} />
-            </div>
+            <Switch>
+                <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+                <Route exact path="/palette/:id" render={() => <h1>Individual palette</h1>} />
+            </Switch>
+            // <div>
+            //     <Palette palette={generatePalette(starterPalettes[4])} />
+            // </div>
         );
     }
 }
