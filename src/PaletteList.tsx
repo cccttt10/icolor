f16725af3423b5fa2e3ef175f92d7f0dd90d54b2
Chuntong Gao <heihegao@gmail.com>
2019-11-06 20:21:28 -0800

    start styling palette list

--- src/PaletteList.tsx
+++ src/PaletteList.tsx
@@ -1,3 +1,4 @@
+import { withStyles } from '@material-ui/styles';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -5,30 +6,59 @@ import MiniPalette from './MiniPalette';
 import { StarterPalette } from './types';
 
 type PaletteListProps = {
+    classes;
     palettes: StarterPalette[];
 };
 
 class PaletteList extends Component<PaletteListProps, {}> {
     render() {
-        const { palettes }: { palettes: StarterPalette[] } = this.props;
+        const {
+            classes,
+            palettes
+        }: { classes; palettes: StarterPalette[] } = this.props;
         return (
-            <div>
-                {/* <MiniPalette /> */}
-                <h1>iColor</h1>
-                {palettes.map((palette: StarterPalette) => (
-                    <MiniPalette {...palette} />
-                    // <p>
-                    //     <Link
-                    //         to={`/palette/${palette.id}`}
-                    //         key={`/palette/${palette.id}`}
-                    //     >
-                    //         {palette.paletteName}
-                    //     </Link>
-                    // </p>
-                ))}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>iColor</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map((palette: StarterPalette) => (
+                            <MiniPalette {...palette} key={palette.id} />
+                        ))}
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default PaletteList;
+export default withStyles({
+    root: {
+        backgroundColor: 'blue',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    container: {
+        width: '50%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column'
+        // flexWrap: 'wrap'
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        color: 'white'
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 30%)',
+        gridGap: '5%'
+    }
+})(PaletteList);
