888dcd681f65874f31d13629590b390990dee834
Chuntong Gao <heihegao@gmail.com>
2019-11-11 11:54:37 -0800

    enhance type checking in PaletteFooter

--- src/components/PaletteFooter/index.tsx
+++ src/components/PaletteFooter/index.tsx
@@ -1,18 +1,22 @@
 import { WithStyles, withStyles } from '@material-ui/styles';
-import React from 'react';
+import React, { ComponentType } from 'react';
 
 import styles from './styles';
 
-interface PaletteFooterProps extends WithStyles<typeof styles> {
+type PaletteFooterProps = {
     paletteName: string;
     emoji: string;
-}
+};
 
-const PaletteFooter = (props: PaletteFooterProps) => (
+const PaletteFooter = (
+    props: PaletteFooterProps & WithStyles<typeof styles>
+): JSX.Element => (
     <footer className={props.classes.footer}>
         {props.paletteName}
         <span className={props.classes.emoji}>{props.emoji}</span>
     </footer>
 );
 
-export default withStyles(styles)(PaletteFooter);
+export default withStyles(styles)(PaletteFooter) as ComponentType<
+    PaletteFooterProps
+>;
