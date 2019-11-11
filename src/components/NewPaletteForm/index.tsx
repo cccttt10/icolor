b2dc7ca5f3c39b944f060500e9d422a99cf8af1f
Chuntong Gao <heihegao@gmail.com>
2019-11-11 13:30:16 -0800

    add Drawer to NewPaletteForm

--- src/components/NewPaletteForm/index.tsx
+++ src/components/NewPaletteForm/index.tsx
@@ -1,13 +1,100 @@
-import React, { Component } from 'react';
+import {
+    AppBar,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    Toolbar,
+    Typography,
+    WithStyles,
+    withStyles
+} from '@material-ui/core';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import MenuIcon from '@material-ui/icons/Menu';
+import classNames from 'classnames';
+import React, { Component, ComponentType } from 'react';
 
-class NewPaletteForm extends Component {
-    render() {
+import styles from './styles';
+
+type NewPaletteFormState = {
+    open: boolean;
+};
+
+class NewPaletteForm extends Component<
+    {} & WithStyles<typeof styles>,
+    NewPaletteFormState
+> {
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
+        this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+        this.handleDrawerClose = this.handleDrawerClose.bind(this);
+    }
+
+    handleDrawerOpen(): void {
+        this.setState({ open: true });
+    }
+
+    handleDrawerClose(): void {
+        this.setState({ open: false });
+    }
+
+    render(): JSX.Element {
+        const { classes } = this.props;
+        const { open }: { open: boolean } = this.state;
+
         return (
-            <div>
-                <h1>New Palette Form!</h1>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: open
+                    })}
+                >
+                    <Toolbar disableGutters={!open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(
+                                classes.menuButton,
+                                open && classes.hide
+                            )}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Persistent drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
+                    <Divider />
+                </Drawer>
+                <main
+                    className={classNames(classes.content, {
+                        [classes.contentShift]: open
+                    })}
+                >
+                    <div className={classes.drawerHeader} />
+                </main>
             </div>
         );
     }
 }
 
-export default NewPaletteForm;
+export default withStyles(styles)(NewPaletteForm) as ComponentType<{}>;
