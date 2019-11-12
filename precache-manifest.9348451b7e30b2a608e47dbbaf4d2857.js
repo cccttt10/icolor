8dde7b7db4dea57ed70a723e4eb76a26b2a51228
Chuntong Gao <heihegao@gmail.com>
2019-11-11 16:46:20 -0800

    Updates

---
+++ precache-manifest.9348451b7e30b2a608e47dbbaf4d2857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0765900abd16ac728f4445e7db832336",
+    "url": "/icolor/index.html"
+  },
+  {
+    "revision": "1d28eb68f928dbdbdf55",
+    "url": "/icolor/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "7ef176c2d5e44d4348e9",
+    "url": "/icolor/static/css/main.0c370c5d.chunk.css"
+  },
+  {
+    "revision": "1d28eb68f928dbdbdf55",
+    "url": "/icolor/static/js/2.ff0e008d.chunk.js"
+  },
+  {
+    "revision": "7ef176c2d5e44d4348e9",
+    "url": "/icolor/static/js/main.a2680960.chunk.js"
+  },
+  {
+    "revision": "843e281dabdbcae1d8cd",
+    "url": "/icolor/static/js/runtime-main.1986d7a6.js"
+  }
+]);
