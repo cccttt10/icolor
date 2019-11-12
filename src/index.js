961185077cd319c79f8455d1268d9dc51c3624b5
Chuntong Gao <heihegao@gmail.com>
2019-11-11 17:05:59 -0800

    add basename prop to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App.tsx';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/icolor">
         <App />
     </BrowserRouter>,
     document.getElementById('root')
